8a08a61687527b9f678872abd615dddb682934bc
Sayed Sinan Umer Jifry <sayedsinan7@gmail.com>
2023-10-27 21:29:01 +0530

    Ui is completely finished

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -58,7 +58,7 @@ class _HomeState extends State<Home> {
                   ),
                 ],
               ),
-              SizedBox(
+            const  SizedBox(
                 height: 20,
               ),
               Row(
@@ -118,8 +118,22 @@ class _HomeState extends State<Home> {
                   ),
                 ],
               ),
-              const SizedBox(
-                height: 10,
+
+
+              Padding(
+                padding: const EdgeInsets.all(12),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    height: 250,
+                    width: 500,
+                    color: Colors.amber,
+                    child:const  Padding(
+                      padding:  EdgeInsets.only(right: 250),
+                      
+                    ),
+                  ),
+                ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
